7c84a19c59156d5a0885ffb0c2c7afadbc7fd38e
Uliss0 <uliss0@live.com>
2022-04-25 07:07:00 -0300

    Agregamos mock, tareas e interfaz

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -13,7 +13,7 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
-    toggleAddTask(){
+  toggleAddTask(){
       console.log("try");
     }
 }
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+export const TASKS=[
+    {
+        id:1,
+        text:"Terminar primer modulo",
+        day: 'Mayo 2022' ,
+        reminder:true},
+
+    {   id:2,
+        text:"Hacer compras",
+        day: 'Abril 26  20 hs' ,
+        reminder:true},
+
+    {   id:3,
+        text:"Agregar Bootstrap",
+        day: 'Mayo 2022' ,
+        reminder:true},
+
+    {   id:4,
+        text:"Leer el psicoanalista",
+        day: 'Junio 2022',
+        reminder:true}
+]
