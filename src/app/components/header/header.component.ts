ce1daa33bcfadb6c7e8bca1c13444fd476505f6c
Uliss0 <uliss0@live.com>
2022-05-02 05:01:17 -0300

    footer agregado

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/service/ui.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,13 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  title:string = 'My Task list v1';
-  constructor() { }
+  title:string = 'My Task List';
+  showAddTask:boolean=false;
+  subscription?:Subscription;
+  
+  constructor(
+    private uiService:UiService,
+    private router:Router
+  ) { 
+    this.subscription=this.uiService.onToggle()
+                          .subscribe(value=>this.showAddTask=value)
+  }
 
+  
   ngOnInit(): void {
   }
 
   toggleAddTask(){
-      console.log("try");
+      this.uiService.toggleAddTask();
     }
+  
+
+  hasRoute(route:string){
+    return this.router.url=== route
+  }
 }
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { takeLast } from 'rxjs';
 import { TaskService } from 'src/app/service/task.service';
 import { Task } from 'src/app/Task';
 
@@ -21,5 +22,26 @@ tasks: Task[]=[];
     (this.tasks=tasks));
   }
 
+    deleteTask(task:Task){
+      this.taskService.deleteTask(task)
+      .subscribe(
+        ()=>(
+        this.tasks = this.tasks.filter( (t) =>{
+        return t.id !== task.id
+      })
+      ))
+      
+    }
+
+    toggleReminder(task:Task){
+      task.reminder = !task.reminder
+     this.taskService.updateTaskReminder(task).subscribe();
+    }
+
+    addTask(task:Task){
+      this.taskService.addTask(task).subscribe((task)=>(
+        this.tasks.push(task)
+      ));
+    }
 }
  
